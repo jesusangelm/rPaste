2f7f3937ce0a9405a06f54d541d7f99f47cfced5
Jesus Marin <jesusangelm@gmail.com>
2014-01-25 21:15:02 -0430

    list rouge supported langs

--- app/helpers/snippets_helper.rb
+++ app/helpers/snippets_helper.rb
@@ -1,2 +1,12 @@
 module SnippetsHelper
+  #Lista los lenguajes soportados por Rouge
+  def lexer_list
+    supported_langs = Array.new
+
+    ugly_list = Rouge::Lexer.all
+    ugly_list.each do |lang|
+      supported_langs << lang.to_s.split("::")[2].downcase
+    end
+    supported_langs
+  end
 end
